40dcc131859a1944dfbcd295909e8ef5c269ab82
Trevor Saudi <isatrevor052@gmail.com>
2020-02-14 20:19:10 +0300

    finalized the project by refactoring

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express  = require('express')
+const Datastore  = require('nedb');
 const app = express(); //creating the web application through express
 app.listen(3000, () => console.log('listening at 3000...'))
 app.use(express.static('public')); //hosting static files  
 app.use(express.json({limit: '1mb'}));
+
+const database = new Datastore('latestnode.db');
+database.loadDatabase();
+
+app.get('/api', (request, response) =>{
+	database.find({}, (err, data) =>{
+		if(err) {
+			response.end();
+			return;
+		}
+	response.json(data);
+})
+});
+
+
+
 app.post('/api' , (request, response) =>{
 	console.log('i got a request')
-	console.log(request.body)
 	const data = request.body;
-	response.json({
-		status: "success",
-		latitude: data.lat,
-		longitude: data.lon
-	});
+	const timestamp = Date.now();
+	data.timestamp = timestamp;
+	database.insert(data);
+	response.json(data);
 
 });
+
